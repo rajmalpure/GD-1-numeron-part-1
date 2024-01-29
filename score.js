6b63149e992e370fc5ad1b921d3aebfa5c4553c5
rajmalpure <malpure.raj@kalvium.community>
2024-01-29 10:12:52 +0530

    push

--- score.js
+++ score.js
@@ -1,1 +1,15 @@
 // Iteration 5: Store the player score and display it on the game over screen
+
+let score = localStorage.getItem("score");
+
+console.log(score)
+
+var scoreBoard = document.getElementById("score-board");
+
+var playAgainBoard = document.getElementById("play-again-button");
+
+scoreBoard.innerHTML = score;
+
+playAgainBoard.onclick = function(){
+    location.href = "./game.html"
+}
